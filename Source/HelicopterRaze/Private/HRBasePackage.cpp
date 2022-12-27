cbb7dae0f6ca1bf94df52580dcc57e0adab65ffc
Kushagra <kushagra.mckv@gmail.com>
2022-12-27 20:42:59 +0530

    KP
    - Removed Debug Package Connectors
    - Added PackageConnector with Two Physics Constraints
    - Added HookSocket to Copter Body
    - Added CableComponent C++ Module
    - Added Logic for Base Package Connector

--- Source/HelicopterRaze/Private/HRBasePackage.cpp
+++ Source/HelicopterRaze/Private/HRBasePackage.cpp
@@ -3,25 +3,78 @@
 
 #include "HRBasePackage.h"
 
-// Sets default values
-AHRBasePackage::AHRBasePackage()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "CableComponent/Classes/CableComponent.h"
 
-}
 
-// Called when the game starts or when spawned
-void AHRBasePackage::BeginPlay()
+AHRBasePackage::AHRBasePackage():
+	CableStartSocketName("CableAttachPoint"),
+	CableFLConnectSocketName("ConnectPoint_FL"),
+	CableFRConnectSocketName("ConnectPoint_FR"),
+	CableRLConnectSocketName("ConnectPoint_RL"),
+	CableRRConnectSocketName("ConnectPoint_RR")
 {
-	Super::BeginPlay();
+ 	PrimaryActorTick.bCanEverTick = true;
+
+	ActorRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	SetRootComponent(ActorRootComponent);
+	
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	BaseMesh->SetupAttachment(ActorRootComponent);
+	
+	ConnectorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ConnectorMesh"));
+	ConnectorMesh->SetupAttachment(ActorRootComponent);
+	ConnectorMesh->SetSimulatePhysics(true);
+	ConnectorMesh->SetCollisionProfileName(FName("PhysicsActor"));
+
+	// Added only Two Physics Constraint By Default to add more add through Blueprints
+	FirstPhyConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("FirstPhyConstraint"));
+	FirstPhyConstraint->SetupAttachment(ActorRootComponent);
+	FirstPhyConstraint->ComponentName1.ComponentName = FName("BaseMesh");
+	FirstPhyConstraint->ComponentName2.ComponentName = FName("ConnectorMesh");
+	FirstPhyConstraint->SetDisableCollision(true);
+	
+	SecondPhyConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("SecondPhyConstraint"));
+	SecondPhyConstraint->SetupAttachment(ActorRootComponent);
+	SecondPhyConstraint->ComponentName1.ComponentName = FName("BaseMesh");
+	SecondPhyConstraint->ComponentName2.ComponentName = FName("ConnectorMesh");
+	SecondPhyConstraint->SetDisableCollision(true);
+
+	CableFL = CreateDefaultSubobject<UCableComponent>(TEXT("CableFL"));
+	CableFL->SetupAttachment(BaseMesh, CableStartSocketName);
+	CableFR = CreateDefaultSubobject<UCableComponent>(TEXT("CableFR"));
+	CableFR->SetupAttachment(BaseMesh, CableStartSocketName);
+	CableRL = CreateDefaultSubobject<UCableComponent>(TEXT("CableRL"));
+	CableRL->SetupAttachment(BaseMesh, CableStartSocketName);
+	CableRR = CreateDefaultSubobject<UCableComponent>(TEXT("CableRR"));
+	CableRR->SetupAttachment(BaseMesh, CableStartSocketName);
+
+	CableFL->AttachEndTo.ComponentProperty = FName("ConnectorMesh");
+	CableFL->AttachEndToSocketName = CableFLConnectSocketName;
+	CableFL->EndLocation = FVector(0.f, 0.f, 0.f);
+	
+	CableFR->AttachEndTo.ComponentProperty = FName("ConnectorMesh");
+	CableFR->AttachEndToSocketName = CableFRConnectSocketName;
+	CableFR->EndLocation = FVector(0.f, 0.f, 0.f);
+	
+	CableRL->AttachEndTo.ComponentProperty = FName("ConnectorMesh");
+	CableRL->AttachEndToSocketName = CableRLConnectSocketName;
+	CableRL->EndLocation = FVector(0.f, 0.f, 0.f);
+	
+	CableRR->AttachEndTo.ComponentProperty = FName("ConnectorMesh");
+	CableRR->AttachEndToSocketName = CableRRConnectSocketName;
+	CableRR->EndLocation = FVector(0.f, 0.f, 0.f);
 	
 }
 
-// Called every frame
 void AHRBasePackage::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
+void AHRBasePackage::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
--- Source/HelicopterRaze/Public/HRBasePackage.h
+++ Source/HelicopterRaze/Public/HRBasePackage.h
@@ -6,21 +6,53 @@
 #include "GameFramework/Actor.h"
 #include "HRBasePackage.generated.h"
 
+class USceneComponent;
+class UStaticMeshComponent;
+class UPhysicsConstraintComponent;
+class UCableComponent;
+
 UCLASS()
 class HELICOPTERRAZE_API AHRBasePackage : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AHRBasePackage();
-
+	virtual void Tick(float DeltaTime) override;
+	
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditDefaultsOnly, Category="Package|SocketNames")
+	FName CableStartSocketName;
+	UPROPERTY(EditDefaultsOnly, Category="Package|SocketNames")
+	FName CableFLConnectSocketName;
+	UPROPERTY(EditDefaultsOnly, Category="Package|SocketNames")
+	FName CableFRConnectSocketName;
+	UPROPERTY(EditDefaultsOnly, Category="Package|SocketNames")
+	FName CableRLConnectSocketName;
+	UPROPERTY(EditDefaultsOnly, Category="Package|SocketNames")
+	FName CableRRConnectSocketName;
 
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Mesh", meta=(AllowPrivateAccess=True))
+	TObjectPtr<USceneComponent> ActorRootComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Mesh", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UStaticMeshComponent> BaseMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Mesh", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UStaticMeshComponent> ConnectorMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Physics", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UPhysicsConstraintComponent> FirstPhyConstraint;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Physics", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UPhysicsConstraintComponent> SecondPhyConstraint;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Rendering", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UCableComponent> CableFL;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Rendering", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UCableComponent> CableFR;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Rendering", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UCableComponent> CableRL;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Package|Rendering", meta=(AllowPrivateAccess=True))
+	TObjectPtr<UCableComponent> CableRR;
+public:
+	
 };
